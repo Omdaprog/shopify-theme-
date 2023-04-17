6ff5a0c4af1d08618c2c618acb472f9359a3fb53
Imed Ben kalia <imedrugby2306@gmail.com>
2023-04-17 15:16:12 +0100

    Update redirect_to_notFoundPage.js

--- assets/redirect_to_notFoundPage.js
+++ assets/redirect_to_notFoundPage.js
@@ -1,13 +1,8 @@
-
-
 const redirection = () => {
   try {
-    console.log("hii")
-    if(
-      document.location.href.includes("https://skratch-studio.myshopify.com") 
-      && 
-      !document.location.href.includes("https://skratch-studio.myshopify.com/not-found")){
-        document.location.href = "https://skratch-studio.myshopify.com/not-found"
+    if( 
+      !document.location.href.includes("not-found")){
+        document.location.href = `${document.location.href}/not-found`
       }
   } catch (error) {
     console.log(error)
